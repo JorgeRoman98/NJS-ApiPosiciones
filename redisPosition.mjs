1170f18ae03bc1c5c0590f956fdfdad520eee077
JorgeRoman98 <jorge.roman@wisetrack.cl>
2024-11-29 16:04:45 -0300

    testeo de conexion 12

--- redisPosition.mjs
+++ redisPosition.mjs
@@ -10,33 +10,26 @@ const client = createClient({
   },
 })
 
-let posRepository;
 
-client.connect().then(() => {
-  const posSchema = new Schema('Position',{
-    "patente": { type: 'string'},
-    "fecha_hora": { type: 'string'},
-    "latitud": { type: 'string'},
-    "longitud": { type: 'string'},
-    "direccion": { type: 'string'},
-    "velocidad": { type: 'string'},
-    "estado_registro": { type: 'string'},
-    "estado_ignicion": { type: 'string'},
-    "numero_evento": { type: 'string'},
-    "odometro": { type: 'string'},
-    "numero_satelites": { type: 'string'},
-    "hdop": { type: 'string'},
-    "edad_dato": { type: 'string'},
-    "rut_conductor": { type: 'string'},
-    "nombre_conductor": { type: 'string'},
-    "opcional_1": { type: 'string'},
-  }, {
-    dataStructure: 'HASH'
-  })
-
-  posRepository = new Repository(posSchema, client)
+client.connect()
+const posSchema = new Schema('Position',{
+  "patente": { type: 'string'},
+  "fecha_hora": { type: 'string'},
+  "latitud": { type: 'string'},
+  "longitud": { type: 'string'},
+  "direccion": { type: 'string'},
+  "velocidad": { type: 'string'},
+  "estado_registro": { type: 'string'},
+  "estado_ignicion": { type: 'string'},
+  "numero_evento": { type: 'string'},
+  "odometro": { type: 'string'},
+  "numero_satelites": { type: 'string'},
+  "hdop": { type: 'string'},
+  "edad_dato": { type: 'string'},
+  "rut_conductor": { type: 'string'},
+  "nombre_conductor": { type: 'string'},
+  "opcional_1": { type: 'string'},
+}, {
+  dataStructure: 'HASH'
 })
-
-
-
-export{ posRepository };
+export const personRepository = client.fetchRepository(posSchema)
