7dc6d766b92aacda18f0537db66a3b36fde39e86
JorgeRoman98 <jorge.roman@wisetrack.cl>
2024-11-29 16:46:30 -0300

    testeo de conexion 23

--- redisPosition.mjs
+++ redisPosition.mjs
@@ -5,8 +5,6 @@ import client from './redisOMClient.mjs'
 
 //class Position extends Entity {}
 
-client.connect()
-
 const posSchema = new Schema('Position',{
   "patente": { type: 'string'},
   "fecha_hora": { type: 'string'},
