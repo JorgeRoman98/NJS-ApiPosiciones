fee56f8de86b77a8954f16463e5bd65843ec0b8f
JorgeRoman98 <jorge.roman@wisetrack.cl>
2024-12-06 17:01:59 -0300

    testeo de conexion 48

--- routes/pos.js
+++ routes/pos.js
@@ -53,6 +53,7 @@ const { createClient } = require('redis');
 const redisserv = process.env.REDIS_HOST || '192.168.2.40';
 const router = express.Router();
 
+console.log(redisserv)
 const redisClient = createClient(url = `redis://${redisserv}:6379`);
 
 (async () => {  redisClient.on('error', (err) => console.log('Redis Client Error', err)); })();
